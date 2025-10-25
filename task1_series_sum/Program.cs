d6ac16c7793f3d6edd90b948eaa8cbcb3983af3e
Zetwyl <r-sherbinin70@mail.ru>
2025-10-25 14:13:20 +0300

    first commit

---
+++ task1_series_sum/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace task1_series_sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int series = 1;
+            int seriesSum = 0;
+            int p = 15;
+            List<int> numbers = new List<int>();
+
+            while (p >= seriesSum)
+            {
+                seriesSum += series;
+                numbers.Add(series);
+                series++;
+            }
+
+            string numbersStr = string.Join($", ", numbers);
+            Console.WriteLine($"Num: {numbersStr}");
+        }
+    }
+}
---
+++ task3_squares_sum/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace task3_squares_sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 111;
+            int squaresSum = 0;
+            int currentNumber = 1;
+            List<int> numbers = new List<int>();
+
+            // Вычислить сумму квадратов ВСЕХ целых чисел, пока сумма < a
+            while (squaresSum + 2 * (currentNumber * currentNumber) < a)
+            {
+                squaresSum += 2 * (currentNumber * currentNumber);
+                currentNumber++;
+            }
+
+            int maxNumber = currentNumber - 1;
+
+            for (int i = -maxNumber; i <= maxNumber; i++)
+            {
+                numbers.Add(i);
+            }
+
+            string result = string.Join($", ", numbers);
+            Console.WriteLine($"Числа: {result}");  // Числа: -2, -1, 0, 1, 2
+            Console.WriteLine($"Сумма квадратов: {squaresSum}");  // Сумма квадратов: 10
+        }
+    }
+}
